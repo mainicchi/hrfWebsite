d534b498487e94dbb64d7abc86ef4a82546718d8
Lassi Ylirautalahti <32744095+Buetix@users.noreply.github.com>
2024-11-17 16:44:04 +0200

    Initializing more files
    Creating files that will be needed

--- src/app/components/textComponent.tsx
+++ src/app/components/textComponent.tsx
@@ -1,1 +1,23 @@
 // TODO: create new component that will hold text and a picture, picture should be able to be on either left or right.
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function TextComponent() {
+    return ( 
+        <div>
+            <div>
+                <p>
+                    "Test"
+                </p>
+            </div>
+        <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt=""
+            width={180}
+            height={38}
+            priority
+            />
+        </div>
+    );
+}
