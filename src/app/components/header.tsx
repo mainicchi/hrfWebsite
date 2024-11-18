24e5a48da55561b2e06cb680fe55365a3b5aa644
Maini <78202734+mainicchi@users.noreply.github.com>
2024-11-18 21:09:12 +0200

    Changed element p to div

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,7 @@ import styles from "./styles/headerStyle.module.css"
 export default function Header() {
     return (
         <div className={styles.header}>
-            <p> - </p>
+          <div>-</div>
         </div>
     );
 }
