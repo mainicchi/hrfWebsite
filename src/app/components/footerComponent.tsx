7f7fc010ba1acc67e45af3c07c52d9fb8aeb1c67
Lassi Ylirautalahti <32744095+Buetix@users.noreply.github.com>
2024-11-25 00:08:15 +0200

    Footer changes

--- src/app/components/footerComponent.tsx
+++ src/app/components/footerComponent.tsx
@@ -1,16 +1,34 @@
 import styles from "./styles/footerStyle.module.css"
+import Link from "next/link"
 
 export default function Footer() {
     return (
-        <footer className={styles.footerContainer}>
-        <div className={styles.footerContent}>
-          <p>© 2024 Your Company. All rights reserved.</p>
-          <ul className="footer-links">
-            <li><a href="/privacy">Privacy Policy</a></li>
-            <li><a href="/terms">Terms of Service</a></li>
-            <li><a href="/contact">Contact Us</a></li>
-          </ul>
+<footer className={styles.footer}>
+      <div className={styles.footerLeft}>
+        <h1 className={styles.footerTitle}>Historic Race Finland ry</h1>
+        <div className={styles.footerSocial}>
+          <Link href="https://facebook.com" target="_blank" rel="noopener noreferrer" className={styles.footerFacebook}>
+            <span className="facebook-icon">F</span> {/* Placeholder for Facebook */}
+          </Link>
         </div>
-      </footer>
+        <p className={styles.footerCopyright}>
+          Oikeudet muutoksiin pidätetään. Viimeisin päivitys 00.00.0000 - email@email.com
+          <br />
+          © Person Person / Historic Race Finland ry. All Rights Reserved.
+        </p>
+      </div>
+      <div style={{width: '30%'}}/>
+      <div className={styles.footerRight}>
+        <ul className={styles.footerLinks}>
+          <li><Link href="/">Ohjelma</Link></li>
+          <li><Link href="/kilpailu">Kilpailu</Link></li>
+          <li><Link href="/rata">Rata ja kartat</Link></li>
+          <li><Link href="/harrasteparkki">Harrasteparkki</Link></li>
+          <li><Link href="/tuotteet">HRF-tuotteet</Link></li>
+          <li><Link href="/kilpailijoille">Kilpailijoille</Link></li>
+          <li><Link href="/medialle">Medialle</Link></li>
+        </ul>
+      </div>
+    </footer>
     );
 }
