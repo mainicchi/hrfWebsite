4ad07b8e57bdc30476117a06293c379149fe417d
Lassi Ylirautalahti <32744095+Buetix@users.noreply.github.com>
2024-11-30 14:27:41 +0200

    Update textCardComponent.tsx

--- src/app/components/textCardComponent.tsx
+++ src/app/components/textCardComponent.tsx
@@ -2,24 +2,27 @@ import styles from "./styles/textCardStyle.module.css"
 
 interface TextCardComponentProps {
     containsLinkButton: boolean
+    buttonText?: string
+    headerText: string
+    bodyText: string
 }
 
 export default function TextCardComponent(props: TextCardComponentProps) {
     return (
         props.containsLinkButton ?
         <div className={styles.card}>
-            <div className={styles.cardHeader}>Fiat Historic Ratapotti
+            <div className={styles.cardHeader}>{props.headerText}
             </div>
-            <div className={styles.cardBody}>Fiat Historic Ratapotti -sarja ajaa muiden ykkösryhmäläisten joukossa. Sen kalusto koostuu huokeasti ylläpidettävistä Fiat 127:sta (ryhmä 1), jotka ovat vuosimalliltaan 1971–81 (Mk1 ja Mk2). Moottorina näissä on 903 cc, jota ei saa virittää. Autot ovat luokitustodistuksen 5415:n sekä sallituiden muutoksien mukaisia.</div>
+            <div className={styles.cardBody}>{props.bodyText}</div>
             <div className={styles.cardLink}>
-                <div className={styles.cardLinkText}>Lisätietoja</div>
+                <div className={styles.cardLinkText}>{props.buttonText}</div>
             </div>
         </div>
         :
         <div className={styles.card}>
-        <div className={styles.cardHeader}>Fiat Historic Ratapotti
+        <div className={styles.cardHeader}>{props.headerText}
         </div>
-        <div className={styles.cardBody}>Fiat Historic Ratapotti -sarja ajaa muiden ykkösryhmäläisten joukossa. Sen kalusto koostuu huokeasti ylläpidettävistä Fiat 127:sta (ryhmä 1), jotka ovat vuosimalliltaan 1971–81 (Mk1 ja Mk2). Moottorina näissä on 903 cc, jota ei saa virittää. Autot ovat luokitustodistuksen 5415:n sekä sallituiden muutoksien mukaisia.</div>
+        <div className={styles.cardBody}>{props.bodyText}</div>
     </div>
     );
 }
