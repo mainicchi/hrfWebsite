d30eadeebc280af4cdc2440ea3cd47e0682ea038
Lassi Ylirautalahti <32744095+Buetix@users.noreply.github.com>
2024-11-30 14:05:28 +0200

    Added link to buttons

--- src/app/components/textCardComponent.tsx
+++ src/app/components/textCardComponent.tsx
@@ -1,7 +1,12 @@
 import styles from "./styles/textCardStyle.module.css"
 
-export default function TextCardComponent() {
+interface TextCardComponentProps {
+    containsLinkButton: boolean
+}
+
+export default function TextCardComponent(props: TextCardComponentProps) {
     return (
+        props.containsLinkButton ?
         <div className={styles.card}>
             <div className={styles.cardHeader}>Fiat Historic Ratapotti
             </div>
@@ -10,5 +15,11 @@ export default function TextCardComponent() {
                 <div className={styles.cardLinkText}>Lisätietoja</div>
             </div>
         </div>
+        :
+        <div className={styles.card}>
+        <div className={styles.cardHeader}>Fiat Historic Ratapotti
+        </div>
+        <div className={styles.cardBody}>Fiat Historic Ratapotti -sarja ajaa muiden ykkösryhmäläisten joukossa. Sen kalusto koostuu huokeasti ylläpidettävistä Fiat 127:sta (ryhmä 1), jotka ovat vuosimalliltaan 1971–81 (Mk1 ja Mk2). Moottorina näissä on 903 cc, jota ei saa virittää. Autot ovat luokitustodistuksen 5415:n sekä sallituiden muutoksien mukaisia.</div>
+    </div>
     );
 }
