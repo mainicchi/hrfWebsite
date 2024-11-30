4cc044122574315dcb90f9eed309db2e1c338ca4
Maini <78202734+mainicchi@users.noreply.github.com>
2024-11-30 17:23:39 +0200

    picture added

--- src/app/components/headerComponent.tsx
+++ src/app/components/headerComponent.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Header() {
     return (
         <div className={styles.header}>
-          <Image src="/images/HGR2024ylapalkki.jpg" alt="Header image" width={0} height={0} sizes="100vw" style={{width: '100%', height: '100%', objectFit: 'fill'}}></Image>
+          <Image src="/images/HRF_2024_ylapalkki.png" alt="Header image" width={0} height={0} sizes="100vw" style={{width: '100%', height: '100%', objectFit: 'fill'}}></Image>
         </div>
     );
 }
